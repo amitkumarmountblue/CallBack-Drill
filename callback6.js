2ea5ebf5f4a8b37a0ab1a069da0068573c771b43
amitkumarmountblue <amit.kumar.17@mountblue.tech>
2021-09-21 12:00:47 +0530

    promise

--- callback6.js
+++ callback6.js
@@ -1,3 +1,9 @@
+/*Problem 6: Write a function that will use the previously written functions to get the following information. You do not need to pass control back to the code that called it.
+
+    Get information from the Thanos boards
+    Get all the lists for the Thanos board
+    Get all cards for all lists simultaneously */
+
 const getBoardInfo = require("./callback1.js");
 const getListBelongToBoard = require("./callback2.js");
 const getCardsUsingListID = require("./callback3.js");
